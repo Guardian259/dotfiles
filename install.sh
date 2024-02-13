237c602121e29d853624db9fd96c0320a7062be3
Guardian <primaryapplogin@gmail.com>
2024-02-13 15:41:15 -0500

    Optimized Install Script

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-
-    $SHELLPROMPTGET
-    $RANGERGET
-    $PACSTALLGET
-
     $EXTENSIONSGET
     $YAKUAKEGET
     $PROGRAMMINGGET
@@ -14,101 +9,86 @@
     $OFFICEGET
     $DESIGNGET
 
+    ####################################
+    ######### Common Functions #########
+    ####################################
+
     function ask() {
         read -p "$1 (Y/n): " response
         [ -z "$response" ] || [ "$response" = "y" ] || [ "$response" = "Y" ]
     }
     #Base Install Function
     function commonProgams() {
-    
-    ENDFLAG=false
-        while [ "$ENDCOMMONFLAG" == false ] 
+        ENDFLAG=false
+            while [ "$ENDCOMMONFLAG" == false ] 
+            do
+                read -p "Please Indicate which modules to include in your base install
+                Then press 4 to install:
+
+                [0] ALL
+
+                [1] Custom Shell Prompt w/ Repo Branch Embed
+                [2] Ranger CLI file manager
+                [3] Pacstall - The AUR for Ubuntu
+
+                [4] Exit Prompt
+                " COMMONFLAG
+                case "${COMMONFLAG}" in
+                    0 ) FLAG=All Suites; ALLCOMMONGET=true ENDCOMMONFLAG=true continue;;
+                    1 ) FLAG=Yakuake Console; SHELLPROMPTGET=true continue;;
+                    2 ) FLAG=Firefox Extensions; RANGERGET=true continue;;
+                    3 ) FLAG=Programming Suite; PACSTALLGET=true continue;;
+                    4 ) FLAG=Exit; ENDFLAG=true continue;
+                esac
+                #Feedback to the user
+                if [ "$SHELLPROMPTGET" == true ] || [ "$ALLCOMMONGET" == true ]; then
+                    echo "The Custom Shell Prompt w/ Repo Branch Embed will be Installed"
+                fi
+                if [ "$RANGERGET" == true ] || [ "$ALLCOMMONGET" == true ]; then
+                    echo "Ranger CLI file manager will be Installed"
+                fi
+                if [ "$PACSTALLGET" == true ] || [ "$ALLCOMMONGET" == true ]; then
+                    echo "Pacstall - The AUR for Ubuntu will be Installed"
+                fi
+            done
+
+        echo "Installing Nala..."
+        apt install nala
+        echo "Installing Neofetch..."
+        apt install neofetch
+        echo "installing Htop..."
+        apt install htop   
+
+        for file in shell/*
         do
-            read -p "Please Indicate which modules to include in your base install
-            Then press 4 to install:
-
-            [0] ALL
-
-            [1] Custom Shell Prompt w/ Repo Branch Embed
-            [2] Ranger CLI file manager
-            [3] Pacstall - The AUR for Ubuntu
-
-            [4] Exit Prompt
-            " COMMONFLAG
-            case "${COMMONFLAG}" in
-                0 ) FLAG=All Suites; ALLCOMMONGET=true ENDCOMMONFLAG=true continue;;
-                1 ) FLAG=Yakuake Console; SHELLPROMPTGET=true continue;;
-                2 ) FLAG=Firefox Extensions; RANGERGET=true continue;;
-                3 ) FLAG=Programming Suite; PACSTALLGET=true continue;;
-                4 ) FLAG=Exit; ENDFLAG=true continue;
-            esac
-            #Feedback to the user
-            if [ "$SHELLPROMPTGET" == true ] || [ "$ALLCOMMONGET" == true ]; then
-                echo "The Custom Shell Prompt w/ Repo Branch Embed will be Installed"
+            fullpath=$(realpath "$file")
+            #Appending Nala onto bashrc
+            if [ "$file" = shell/nala.sh ]; then
+                echo "Appending ${file} into ~/.bashrc..."
+                echo "source $fullpath" >> ~/.bashrc
+                echo "Appending ${file} into /root/.bashrc..."
+                echo "source $fullpath" >> /etc/bash.bashrc
             fi
-            if [ "$RANGERGET" == true ] || [ "$ALLCOMMONGET" == true ]; then
-                echo "Ranger CLI file manager will be Installed"
-            fi
-            if [ "$PACSTALLGET" == true ] || [ "$ALLCOMMONGET" == true ]; then
-                echo "Pacstall - The AUR for Ubuntu will be Installed"
+            #
+            if [ "$file" != shell/nala.sh ] && [ "$SHELLPROMPTGET" == true ]; then
+                echo "Appending ${file} into ~/.bashrc..."
+                echo "source $fullpath" >> ~/.bashrc
             fi
         done
-
-    echo "Installing Nala..."
-    apt install nala
-    echo "Installing Neofetch..."
-    apt install neofetch
-    echo "installing Htop..."
-    apt install htop   
-
-    for file in shell/*
-    do
-        fullpath=$(realpath "$file")
-        #Appending Nala onto bashrc
-        if [ "$file" = shell/nala.sh ]; then
-            echo "Appending ${file} into ~/.bashrc..."
-            echo "source $fullpath" >> ~/.bashrc
-            echo "Appending ${file} into /root/.bashrc..."
-            echo "source $fullpath" >> /etc/bash.bashrc
+        if [ "$RANGERGET" == true ]; then
+            echo "Installing Ranger..."
+            apt install ranger
         fi
-        #
-        if [ "$file" != shell/nala.sh ] && [ "$SHELLPROMPTGET" == true ]; then
-            echo "Appending ${file} into ~/.bashrc..."
-            echo "source $fullpath" >> ~/.bashrc
-        fi
-    done
-    if [ "$RANGERGET" == true ]; then
-        echo "Installing Ranger..."
-        apt install ranger
-    fi
 
-    if [ "$PACSTALLGET" == true ]; then
-        echo "Installing PacStall..."
-        bash -c "$(curl -fsSL https://pacstall.dev/q/install)"
-    fi
+        if [ "$PACSTALLGET" == true ]; then
+            echo "Installing PacStall..."
+            bash -c "$(curl -fsSL https://pacstall.dev/q/install)"
+        fi
     }
 
-    function getProtonGE() {
-        echo "Installing GE-Proton..."
-        # make temp working directory
-        mkdir /tmp/proton-ge-custom
-        cd /tmp/proton-ge-custom || return
-        # download  tarball
-        curl -sLOJ "$(curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep browser_download_url | cut -d\" -f4 | grep .tar.gz)"
-        # download checksum
-        curl -sLOJ "$(curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep browser_download_url | cut -d\" -f4 | grep .sha512sum)"
-        # check tarball with checksum
-            if sha512sum -c ./*.sha512sum; then # if result is ok, continue
-                # make steam directory if it does not exist
-                mkdir -p ~/.steam/root/compatibilitytools.d
-                # extract proton tarball to steam directory
-                tar -xf GE-Proton*.tar.gz -C ~/.steam/root/compatibilitytools.d/
-                cd ..
-                echo "All done :)"
-            else
-                echo "Checksum mismatch exiting..."
-            fi
-    }
+    ####################################
+    ######### Server Functions #########
+    ####################################
 
     function getDocker() {
         # Docker Engine's Instalation
@@ -127,6 +107,32 @@
         apt update
         apt install docker-compose
         apt install docker-ce docker-ce-cli docker-compose-plugin
+    }
+
+    ####################################
+    ######## Desktop Functions #########
+    ####################################
+
+    function getProtonGE() {
+    echo "Installing GE-Proton..."
+    # make temp working directory
+    mkdir /tmp/proton-ge-custom
+    cd /tmp/proton-ge-custom || return
+    # download  tarball
+    curl -sLOJ "$(curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep browser_download_url | cut -d\" -f4 | grep .tar.gz)"
+    # download checksum
+    curl -sLOJ "$(curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep browser_download_url | cut -d\" -f4 | grep .sha512sum)"
+    # check tarball with checksum
+        if sha512sum -c ./*.sha512sum; then # if result is ok, continue
+            # make steam directory if it does not exist
+            mkdir -p ~/.steam/root/compatibilitytools.d
+            # extract proton tarball to steam directory
+            tar -xf GE-Proton*.tar.gz -C ~/.steam/root/compatibilitytools.d/
+            cd ..
+            echo "All done :)"
+        else
+            echo "Checksum mismatch exiting..."
+        fi
     }
 
     function getGaming() {
@@ -277,9 +283,9 @@
             for file in firfox/*
             do
                 echo "Symlinking Firefox Extensions List into /etc/firefox/ ..."
-                ln -s "$(realpath "policies.json")" /etc/firefox/
+                ln -s "$(realpath "$file")" /etc/firefox/
             done
-        fi
+        fi    
         apt install thunderbird
         if [ "$PROGRAMMINGGET" == true ] || [ "$ALLGET" == true ]; then
             getCoding
@@ -314,7 +320,7 @@
         if $ENVIORMENT != 4; then
             if ask "You have chosen $ENVIORMENT are you sure?"; then
                 echo "Installing $ENVIORMENT Enviorment..."
-                commonProgams
+                install/common-install.sh
                 case "${ENVIORMENT}" in
                     1 ) ENVIORMENT=Server; serverEn return;;
                     2 ) ENVIORMENT=Desktop; desktopEn return;;
