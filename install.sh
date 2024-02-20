bdefcf68f8b2fe580666d9b6256470e26631a2d8
Guardian <primaryapplogin@gmail.com>
2024-02-20 15:07:14 -0500

    Included Jellyfin Media Player Version in echo command for clarity

--- install.sh
+++ install.sh
@@ -215,6 +215,7 @@
         #Installing Jellyfin Media Player
         echo "Installing Jellyfin Mdeia Player..."
         jellyfin_version=$(curl --head https://github.com/jellyfin/jellyfin-media-player/releases/latest | tr -d '\r' | grep '^location' | sed 's/.*\/v//g')
+        echo "Installing Jellyfin Mdeia Player $jellyfin_version..."
         wget "https://github.com/jellyfin/jellyfin-media-player/releases/download/v$jellyfin_version/jellyfin-media-player_$jellyfin_version-1_amd64-$(grep VERSION_CODENAME /etc/os-release | cut -d= -f2).deb" -O jellyfin-media-player-"$jellyfin_version".deb
         apt install -y ./jellyfin-media-player-"$jellyfin_version".deb
         rm jellyfin-media-player-"$jellyfin_version".deb
