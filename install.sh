f929984ad9d328f72c541960d74a5c92174cc71c
Guardian <PrimaryAppLogin@gmail.com>
2024-02-21 20:52:06 -0600

    Fixed bugg in Jellyfin install due to distro version missmatch

--- install.sh
+++ install.sh
@@ -253,7 +253,7 @@
         echo "Installing Jellyfin Mdeia Player..."
         jellyfin_version=$(curl --head https://github.com/jellyfin/jellyfin-media-player/releases/latest | tr -d '\r' | grep '^location' | sed 's/.*\/v//g')
         echo "Installing Jellyfin Mdeia Player $jellyfin_version..."
-        wget "https://github.com/jellyfin/jellyfin-media-player/releases/download/v$jellyfin_version/jellyfin-media-player_$jellyfin_version-1_amd64-$(grep VERSION_CODENAME /etc/os-release | cut -d= -f2).deb" -O jellyfin-media-player-"$jellyfin_version".deb
+        wget "https://github.com/jellyfin/jellyfin-media-player/releases/download/v$jellyfin_version/jellyfin-media-player_$jellyfin_version-1_amd64-jammy.deb" -O jellyfin-media-player-"$jellyfin_version".deb
         nala install -y ./jellyfin-media-player-"$jellyfin_version".deb
         rm jellyfin-media-player-"$jellyfin_version".deb
         #Installing Sonixd
